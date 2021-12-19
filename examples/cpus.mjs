ef8e593880f602d577b679a480917aa054196ef0
aminnairi <18418459+aminnairi@users.noreply.github.com>
2021-12-19 20:08:18 +0100

    initial commit

---
+++ examples/cpus.mjs
@@ -0,0 +1,52 @@
+import {cpus} from "os";
+import {createInterface} from "readline/promises";
+import {stdin as input, stdout as output} from "process";
+import {createRenderer} from "@aminnairi/i3status";
+
+const getCpus = () => {
+  const allCpus = cpus();
+  const count = allCpus.length;
+  const speed = Math.round(allCpus.reduce((clock, cpu) => clock + cpu.speed, 0) / count) / 1000;
+
+  return `${count} CPUs ${speed}GHz`;
+};
+
+const render = createRenderer({
+  createInterface,
+  input,
+  output
+});
+
+const dispatch = render({
+  initialBlocks: [
+    {
+      name: "cpus",
+      full_text: getCpus()
+    }
+  ],
+  onDispatch: ({blocks, action: {name, payload}}) => {
+    switch (name) {
+      case "CPUS_SET_FULL_TEXT":
+        return blocks.map(block => {
+          if (block.name !== "cpus") {
+            return block;
+          }
+
+          return {
+            ...block,
+            full_text: payload
+          };
+        })
+
+      default:
+        return blocks;
+    }
+  }
+});
+
+setInterval(() => {
+  dispatch({
+    name: "CPUS_SET_FULL_TEXT",
+    payload: getCpus()
+  });
+}, 1000);
